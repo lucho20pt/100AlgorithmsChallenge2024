3e3ef05438c36b80c3aa419d124798673cbff45d
Daniel Batista <dani_batista20@hotmail.com>
2024-05-02 19:01:42 +0100

    add: 10_adjacentElementsProduct

--- src/10_adjacentElementsProduct/adjacentElementsProduct.ts
+++ src/10_adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,13 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-    
+  let output = 0
+  for (let i = 0; i < inputArray.length; i++) {
+    const el = inputArray[i] * inputArray[i + 1]
+    if (output <= el) {
+      output = el
+    }
+    // console.log(output)
+  }
+  return output
 }
 
-// console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
