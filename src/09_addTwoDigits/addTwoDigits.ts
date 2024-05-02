9db20899fca53e6630c7bf097bae940950e8ace9
Daniel Batista <dani_batista20@hotmail.com>
2024-05-02 18:38:56 +0100

    add: 09_addTwoDigits

--- src/09_addTwoDigits/addTwoDigits.ts
+++ src/09_addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,13 @@
-export function addTwoDigits(n: any): number {
-    
+export function addTwoDigits(n: number): number {
+  const strArr = n.toString().split('')
+  //   console.log(strArr)
+  const initialValue = 0
+  const sum = strArr.reduce(
+    (acc, curr) => Number(acc) + Number(curr),
+    initialValue
+  )
+  //   console.log(sum)
+  return sum
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29))
