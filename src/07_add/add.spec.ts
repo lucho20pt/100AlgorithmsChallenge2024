1257e30b31db337526fdf277e309704b2e723872
Daniel Batista <dani_batista20@hotmail.com>
2024-05-02 17:00:28 +0100

    add: 07_add - addAllNum

--- src/07_add/add.spec.ts
+++ src/07_add/add.spec.ts
@@ -1,6 +1,24 @@
-import { add } from './add'
+import { add, addAllNum } from './add'
 
 describe(add.name, () => {
+  it('test 1', () => {
+    // act
+    const result = add(1, 2)
+
+    // assert
+    expect(result).toBe(3)
+  })
+
+  it('test 2', () => {
+    // act
+    const result = add(3, 2)
+
+    // assert
+    expect(result).toBe(5)
+  })
+})
+
+describe(addAllNum.name, () => {
   it('test 1', () => {
     // act
     const result = add(1, 2)
--- src/07_add/add.ts
+++ src/07_add/add.ts
@@ -2,5 +2,16 @@ export function add(param1: number, param2: number): number {
   return param1 + param2
 }
 
+export function addAllNum(...params: number[]): number {
+  let sum: number = 0
+  params.forEach((el) => {
+    sum += el
+  })
+  return sum
+}
+
 console.log(add(1, 2))
 console.log(add(3, 2))
+
+console.log(addAllNum(1, 2, 4))
+console.log(addAllNum(3, 2))
