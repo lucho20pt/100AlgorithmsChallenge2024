ab58318713d0b24d8db8b9577d6e625d4beabd5d
Daniel Batista <dani_batista20@hotmail.com>
2024-05-03 10:35:52 +0100

    add: 11_allLongestStrings

--- src/11_allLongestStrings/allLongestStrings.ts
+++ src/11_allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,20 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestString = 0
+  const arrLongestStrings: string[] = []
+
+  inputArray.forEach((value) => {
+    longestString = value.length > longestString ? value.length : longestString
+  })
+
+  inputArray.forEach((el) => {
+    if (el.length === longestString) {
+        arrLongestStrings.push(el)
+    }
+  })
+
+  return arrLongestStrings
 
 }
 
-// console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']))
+console.log(allLongestStrings(['z', 'aaa', 'ad', 'vcd', 'aba']))
