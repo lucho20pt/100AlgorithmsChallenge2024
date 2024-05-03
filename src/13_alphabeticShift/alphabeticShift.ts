c028759c33028c3c2cf12547da24a93711c3d9e7
Daniel Batista <dani_batista20@hotmail.com>
2024-05-03 14:22:51 +0100

    add: 13_alphabeticShift

--- src/13_alphabeticShift/alphabeticShift.ts
+++ src/13_alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,21 @@
 export function alphabeticShift(inputString: string): string {
+  const alphabet = [...Array(26)].map((_, index) =>
+    String.fromCharCode(index + 97)
+  )
+  const inputStringArr = inputString.split('')
+  const newStringArr: string[] = []
 
+  inputStringArr.forEach((value) => {
+    const index = alphabet.findIndex((el) => el.includes(value))
+    let letter = alphabet.at(index + 1)
+
+    if (letter === undefined) {
+      letter = 'a'
+    }
+    newStringArr.push(letter)
+  })
+
+  return newStringArr.join('')
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('crazy'))
