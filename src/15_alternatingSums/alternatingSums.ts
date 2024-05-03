19395fe522547325680a80632639b4772f0d2282
Daniel Batista <dani_batista20@hotmail.com>
2024-05-03 20:07:36 +0100

    add: 15_alternatingSums

--- src/15_alternatingSums/alternatingSums.ts
+++ src/15_alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 export function alternatingSums(a: number[]): number[] {
+  const evenArr: number[] = []
+  const oddArr: number[] = []
 
+  a.forEach((el, index) => {
+    const isEven = index % 2 === 0
+    isEven ? evenArr.push(el) : oddArr.push(el)
+  })
+
+  const even = evenArr.reduce((acc, curr) => acc + curr, 0)
+  const odd = oddArr.reduce((acc, curr) => acc + curr, 0)
+
+  return [even, odd]
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
