20134ebe7a7aa0c04a82a99e7d5a8e6465273559
Daniel Batista <dani_batista20@hotmail.com>
2024-05-06 21:13:28 +0100

    add: 17_areSimilar

--- src/17_areSimilar/areSimilar.ts
+++ src/17_areSimilar/areSimilar.ts
@@ -1,7 +1,18 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+  if (a.length !== b.length) return false
 
+  // copy and sort arr
+  const A = a.slice().sort()
+  const B = b.slice().sort()
+
+  return A.every((el, i) => {
+    const j = B[i]
+    const result = el - j
+    if (result !== 0) return false
+    return true
+  })
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3, 10], [1, 2, 3, 10]))
+console.log(areSimilar([1, 2, 3], [2, 1, 3]))
+console.log(areSimilar([1, 2, 2], [2, 1, 1]))
