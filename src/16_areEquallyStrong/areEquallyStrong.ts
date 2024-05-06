b1e1318128bfe1e7c3fc2e9bee63f525d205b720
Daniel Batista <dani_batista20@hotmail.com>
2024-05-06 17:25:14 +0100

    add: 16_areEquallyStrong

--- src/16_areEquallyStrong/areEquallyStrong.ts
+++ src/16_areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,23 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendLeft: number,
+  friendRight: number
+): boolean {
+  // check witch arm is strong or weak
+  const yourStrongArm = yourLeft > yourRight ? yourLeft : yourRight
+  const yourWeakArm = yourLeft < yourRight ? yourLeft : yourRight
+  const friendsStrongArm = friendLeft > friendRight ? friendLeft : friendRight
+  const friendsWeakArm = friendLeft < friendRight ? friendLeft : friendRight
 
+  // compare arms with friend
+  if (yourStrongArm === friendsStrongArm && yourWeakArm === friendsWeakArm) {
+    return true
+  } else {
+    return false
+  }
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
