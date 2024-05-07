13447a5d65853030641c8304817dd476691a7b94
Daniel Batista <dani_batista20@hotmail.com>
2024-05-07 01:26:41 +0100

    add: 18_arrayChange

--- src/18_arrayChange/arrayChange.ts
+++ src/18_arrayChange/arrayChange.ts
@@ -1,5 +1,21 @@
 export function arrayChange(inputArray: number[]): number {
+  let sum = 0
+  const arr = [...inputArray]
 
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i]
+    let last = arr[i - 1]
+
+    if (last >= el) {
+      let diff = last - el + 1
+      sum += diff
+      arr[i] = el + diff
+      diff = 0
+    }
+  }
+
+  return sum
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]))
+console.log(arrayChange([2, 1, 1, 1]))
