f837b386b376dbe77a866266b6cd3a18d31e32c3
Daniel Batista <dani_batista20@hotmail.com>
2024-05-08 00:34:56 +0100

    add: 23_arrayReplace

--- src/23_arrayReplace/arrayReplace.ts
+++ src/23_arrayReplace/arrayReplace.ts
@@ -1,5 +1,20 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  const arr: number[] = []
+  inputArray
+  for (let i = 0; i < inputArray.length; i++) {
+    let el = inputArray[i]
+    el
 
+    el === elemToReplace
+      ? arr.push((inputArray[i] = substitutionElem))
+      : arr.push(inputArray[i])
+  }
+  
+  return arr
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3))
