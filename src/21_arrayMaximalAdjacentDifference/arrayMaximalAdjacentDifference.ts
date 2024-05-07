b8f6c54fa6aab50b43f1abe0e30425e78f3ad926
Daniel Batista <dani_batista20@hotmail.com>
2024-05-07 23:15:54 +0100

    add: 21_arrayMaximalAdjacentDifference

--- src/21_arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ src/21_arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,13 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let max = 0
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    let temp = Math.abs(inputArray[i]) - Math.abs(inputArray[i + 1])
+
+    max < temp ? (max = temp) : max
+  }
+
+  return max
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]))
