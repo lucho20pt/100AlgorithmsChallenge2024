41e5b8f99b5e83554d3bbd155539a73e20ec0bf8
Daniel Batista <dani_batista20@hotmail.com>
2024-05-07 19:54:30 +0100

    add: 19_arrayConversion

--- src/19_arrayConversion/arrayConversion.ts
+++ src/19_arrayConversion/arrayConversion.ts
@@ -1,5 +1,32 @@
 export function arrayConversion(inputArray: number[]): number {
+  let arr: number[] = inputArray.slice()
+  let isEven: boolean = false
+  let length = arr.length
 
+  const sum = (a: number, b: number) => a + b
+  const prod = (a: number, b: number) => a * b
+
+  const recursive = (count: number) => {
+    if (count > 1) recursive(count - 1)
+    if (arr.length === 1) return arr[0]
+
+    isEven = !isEven
+    let temp: number[] = []
+
+    if (isEven) {
+      for (let i = 0; i < arr.length; i += 2) {
+        temp.push(sum(arr[i], arr[i + 1]))
+      }
+      arr = temp
+    } else {
+      for (let i = 0; i < arr.length; i += 2) {
+        temp.push(prod(arr[i], arr[i + 1]))
+      }
+      arr = temp
+    }
+  }
+  recursive(length)
+  return arr[0]
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]))
