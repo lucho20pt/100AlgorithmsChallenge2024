bae72ad62800b147f4bc8bf41c8271b1023793df
Daniel Batista <dani_batista20@hotmail.com>
2024-05-07 22:17:42 +0100

    add: 20_arrayMaxConsecutiveSum

--- src/20_arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ src/20_arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,19 @@
-export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+export function arrayMaxConsecutiveSum(
+  inputArray: number[],
+  k: number
+): number {
+  let max: number = 0
+  const sum = (...params: number[]) => {
+    return params.reduce((a, b) => a + b, 0)
+  }
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    const temp = inputArray.slice(i, i + k)
+    let res = sum(...temp)
+    max < res ? (max = res) : res
+  }
+
+  return max
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2))
