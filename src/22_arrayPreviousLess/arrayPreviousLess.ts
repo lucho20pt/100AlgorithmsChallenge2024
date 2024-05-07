a1e4cbb6bab0f6d4bd61cfe63f3b12d687d282c9
Daniel Batista <dani_batista20@hotmail.com>
2024-05-08 00:08:33 +0100

    add: 22_arrayPreviousLess

--- src/22_arrayPreviousLess/arrayPreviousLess.ts
+++ src/22_arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,11 @@
 export function arrayPreviousLess(items: number[]): number[] {
-
+  const arr: number[] = []
+  for (let i = 0; i < items.length; i++) {
+    let el = items[i]
+    let prev = items[i - 1]
+    el > prev ? arr.push(prev) : arr.push(-1)
+  }
+  return arr
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]))
