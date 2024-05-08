694b0e7f7a5d68459b287bcb24022ce786b6b59b
Daniel Batista <dani_batista20@hotmail.com>
2024-05-08 18:47:22 +0100

    add: 24_avoidObstacles - refactor to while()

--- src/24_avoidObstacles/avoidObstacles.ts
+++ src/24_avoidObstacles/avoidObstacles.ts
@@ -1,24 +1,50 @@
 export function avoidObstacles(inputArray: number[]): number {
   const arr = inputArray.sort((a, b) => a - b)
   let jump = 1
-  let loops = 0
+  let counter = 0
 
-  for (let i = 0; i < arr.length; i++) {
-    let path = i * jump
-    path
+  while (true) {
+    let path = counter * jump
     let jumpIsNotClear = arr.every((el) => el !== path)
 
     if (!jumpIsNotClear) {
       // if jump not clear try next jump++ and repeat
-      i = 0
+      counter = 0
       jump++
+    } else {
+      counter++
     }
-    loops++
+
+    if (counter > arr.length) {
+      break
+    }
   }
-  console.log(loops)
+
   return jump
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]))
 console.log(avoidObstacles([5, 3, 6, 7, 9, 20]))
 console.log(avoidObstacles([1, 2, 3, 4, 5, 6, 7, 8]))
+
+// with for loop
+// export function avoidObstacles(inputArray: number[]): number {
+//   const arr = inputArray.sort((a, b) => a - b)
+//   let jump = 1
+//   let loops = 0
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let path = i * jump
+//     path
+//     let jumpIsNotClear = arr.every((el) => el !== path)
+
+//     if (!jumpIsNotClear) {
+//       // if jump not clear try next jump++ and repeat
+//       i = 0
+//       jump++
+//     }
+//     loops++
+//   }
+//   console.log(loops)
+//   return jump
+// }
