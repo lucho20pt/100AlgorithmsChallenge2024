942a74c779afb07e3b87ef9254be0c9dbc643fdf
Daniel Batista <dani_batista20@hotmail.com>
2024-05-09 19:15:59 +0100

    add: 30_characterParity

--- src/30_characterParity/characterParity.ts
+++ src/30_characterParity/characterParity.ts
@@ -1,7 +1,29 @@
+// /* my first solution */
 export function characterParity(symbol: string): string {
-
+  const isNumber = (str: string) => !isNaN(Number(str))
+  if (isNumber(symbol)) {
+    return parseInt(symbol) % 2 === 0 ? 'even' : 'odd'
+  } else {
+    return 'not a digit'
+  }
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
+
+/* my second solution */
+// export function characterParity(symbol: string): string {
+//   const isNumber = (str: string): boolean => {
+//     return !isNaN(Number(str))
+//   }
+//   const isOddEvenNumber = (str: string): string => {
+//     if (isNumber(str)) {
+//       const isEven = parseInt(str) % 2 === 0
+//       return isEven ? 'even' : 'odd'
+//     } else {
+//       return 'not a digit'
+//     }
+//   }
+//   return isOddEvenNumber(symbol)
+// }
