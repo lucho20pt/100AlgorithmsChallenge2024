16b9d678facaa369840e90ad931f42f763cd4675
Daniel Batista <dani_batista20@hotmail.com>
2024-05-09 16:17:11 +0100

    add: 26_boxBlur

--- src/26_boxBlur/boxBlur.spec.ts
+++ src/26_boxBlur/boxBlur.spec.ts
@@ -1,16 +1,36 @@
-import { boxBlur } from './boxBlur';
+import { boxBlur } from './boxBlur'
 
-xdescribe(boxBlur.name, () => {
-    it('Test 1', () => {
-        // arrange
-        const data = [[1, 1, 1],
-        [1, 7, 1],
-        [1, 1, 1]];
+describe(boxBlur.name, () => {
+  it('Test 1', () => {
+    // arrange
+    const data = [
+      [1, 1, 1],
+      [1, 7, 1],
+      [1, 1, 1]
+    ]
 
-        // act
-        const response = boxBlur(data);
+    // act
+    const response = boxBlur(data)
 
-        // assert
-        expect(response).toEqual([[1]]);
-    });
-});
+    // assert
+    expect(response).toEqual([[1]])
+  })
+  it('Test 2', () => {
+    // arrange
+    const data = [
+      [7, 4, 0, 1],
+      [5, 6, 2, 2],
+      [6, 10, 7, 8],
+      [1, 4, 2, 0]
+    ]
+
+    // act
+    const response = boxBlur(data)
+
+    // assert
+    expect(response).toEqual([
+      [5, 4],
+      [4, 4]
+    ])
+  })
+})
--- src/26_boxBlur/boxBlur.ts
+++ src/26_boxBlur/boxBlur.ts
@@ -1,7 +1,32 @@
 export function boxBlur(image: number[][]): number[][] {
+  let res: number[][] = []
 
+  // sum inner arr
+  for (let i = 1; i < image.length - 1; i++) {
+    const row = []
+    for (let j = 1; j < image[i].length - 1; j++) {
+      // => 7
+      let count = 0
+      let sum = 0
+
+      for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+          sum += image[i + x][j + y]
+          count++
+        }
+      }
+      row.push(Math.floor(sum / count))
+    }
+    res.push(row)
+  }
+
+  return res
 }
 
-// console.log(boxBlur([[1, 1, 1], 
-//     [1, 7, 1], 
-//     [1, 1, 1]]));
+console.log(
+  boxBlur([
+    [1, 1, 1],
+    [1, 7, 1],
+    [1, 1, 1]
+  ])
+)
