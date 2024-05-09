b89824ed316a513d45475e2162b1b42dbaee52d3
Daniel Batista <dani_batista20@hotmail.com>
2024-05-09 23:21:20 +0100

    add: 33_chunkyMonkey

--- src/33_chunkyMonkey/chunkyMonkey.ts
+++ src/33_chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,10 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
+  let monkey = (a: any[], s: number) => {
+    return Array(Array(...a.slice(0, s)), Array(...a.slice(s)))
+  }
 
+  return monkey(arr, size)
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2))
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4))
