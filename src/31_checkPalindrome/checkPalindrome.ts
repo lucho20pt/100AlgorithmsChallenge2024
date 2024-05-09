5de2c017d650d16362eeb9dd31a47c619a588977
Daniel Batista <dani_batista20@hotmail.com>
2024-05-09 19:24:21 +0100

    add: 31_checkPalindrome

--- src/31_checkPalindrome/checkPalindrome.ts
+++ src/31_checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,8 @@
 export function checkPalindrome(inputString: string): boolean {
-
+  const str = inputString.split('').reverse().join('').toLocaleLowerCase()
+  return str === inputString
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa'))
+console.log(checkPalindrome('abac'))
+console.log(checkPalindrome('a'))
