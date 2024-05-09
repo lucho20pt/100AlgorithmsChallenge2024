5b44c2bd41d24ee4f31fccab069fbfb28a7e266f
Daniel Batista <dani_batista20@hotmail.com>
2024-05-09 17:33:31 +0100

    add: 29_centuryFromYear

--- src/29_centuryFromYear/centuryFromYear.ts
+++ src/29_centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,8 @@
 export function centuryFromYear(year: number): number {
-
+  return Math.ceil(year / 100)
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(101))
+console.log(centuryFromYear(1))
+console.log(centuryFromYear(1905))
+console.log(centuryFromYear(1700))
