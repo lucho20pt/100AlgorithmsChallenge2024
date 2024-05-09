f5eed38e5f9da10e6242fe15a05b20b822ba3f5a
Daniel Batista <dani_batista20@hotmail.com>
2024-05-09 17:17:45 +0100

    add: 28_caseInsensitivePalimdrome

--- src/28_caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ src/28_caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,7 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  let temp = inputString.split('').reverse().join('').toLocaleLowerCase()
+  return temp === inputString.toLocaleLowerCase()
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'))
+console.log(isCaseInsensitivePalindrome('aBac'))
