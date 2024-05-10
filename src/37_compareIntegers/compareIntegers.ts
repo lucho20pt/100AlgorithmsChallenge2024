a91e4b24fa14937ba9ff8fe795edbc30e496d7e6
Daniel Batista <dani_batista20@hotmail.com>
2024-05-10 17:49:50 +0100

    add: 37_compareIntegers

--- src/37_compareIntegers/compareIntegers.ts
+++ src/37_compareIntegers/compareIntegers.ts
@@ -1,7 +1,16 @@
 export function compareIntegers(a: string, b: string): string {
+  const n1 = parseInt(a)
+  const n2 = parseInt(b)
 
+  if (n1 < n2) {
+    return 'less'
+  } else if (n1 > n2) {
+    return 'greater'
+  } else {
+    return 'equal'
+  }
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'))
+console.log(compareIntegers('875', '799'))
+console.log(compareIntegers('1000', '1000'))
