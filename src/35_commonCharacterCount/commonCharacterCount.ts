0538e6efe02bc6b0dc88d1bf7141483a9479f6a3
Daniel Batista <dani_batista20@hotmail.com>
2024-05-10 16:32:36 +0100

    upd: 35_commonCharacterCount - refactor

--- src/35_commonCharacterCount/commonCharacterCount.ts
+++ src/35_commonCharacterCount/commonCharacterCount.ts
@@ -7,28 +7,51 @@
  */
 
 export function commonCharacterCount(s1: string, s2: string): number {
-  const arr1 = s1.split('')
-  const arr2 = s2.split('')
+  const objCounter: { [key: string]: number } = {}
+  let counter = 0
 
-  const obj: { [key: string]: number } = {}
-  let counter: number = 0
-
-  arr1.forEach((letter) => {
-    if (obj.hasOwnProperty(letter)) {
-      obj[letter] += 1
+  for (let char of s1) {
+    if (objCounter[char]) {
+      objCounter[char]++
     } else {
-      obj[letter] = 1
+      objCounter[char] = 1
     }
-  })
+  }
 
-  arr2.forEach((letter) => {
-    if (obj.hasOwnProperty(letter) && obj[letter] > 0) {
-      counter += 1
-      obj[letter] -= 1
+  for (let char of s2) {
+    if (objCounter[char] > 0) {
+      objCounter[char]--
+      counter++
     }
-  })
+  }
 
   return counter
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'))
+
+// /* using split() & hasOwnProperty*/
+// export function commonCharacterCount(s1: string, s2: string): number {
+//   const arr1 = s1.split('')
+//   const arr2 = s2.split('')
+
+//   const obj: { [key: string]: number } = {}
+//   let counter: number = 0
+
+//   arr1.forEach((letter) => {
+//     if (obj.hasOwnProperty(letter)) {
+//       obj[letter] += 1
+//     } else {
+//       obj[letter] = 1
+//     }
+//   })
+
+//   arr2.forEach((letter) => {
+//     if (obj.hasOwnProperty(letter) && obj[letter] > 0) {
+//       counter += 1
+//       obj[letter] -= 1
+//     }
+//   })
+
+//   return counter
+// }
