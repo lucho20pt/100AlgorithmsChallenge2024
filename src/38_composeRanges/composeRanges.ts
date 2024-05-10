b2e43c5d6b946ed04cc2e9b7eafe2f9e5146afae
Daniel Batista <dani_batista20@hotmail.com>
2024-05-10 22:01:32 +0100

    add: 38_composeRanges

--- src/38_composeRanges/composeRanges.ts
+++ src/38_composeRanges/composeRanges.ts
@@ -1,5 +1,32 @@
 export function composeRanges(nums: number[]): string[] {
+  const arr: string[] = []
+  let temp: number[] = []
 
+  for (let i = 0; i < nums.length; i++) {
+    let el = nums[i]
+    let prev = nums[i - 1]
+    let next = nums[i + 1]
+
+    // get single
+    if (prev + 1 !== el && next - 1 !== el) {
+      temp.push(el)
+      let srt = `${temp[0]}`
+      arr.push(srt)
+      temp = []
+    }
+
+    // get range
+    if (prev + 1 === el || next - 1 === el) {
+      temp.push(el)
+      if (el + 1 !== next) {
+        let srt = `${temp[0]}->${temp[temp.length - 1]}`
+        arr.push(srt)
+        temp = []
+      }
+    }
+  }
+
+  return arr
 }
 
-// console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]))
