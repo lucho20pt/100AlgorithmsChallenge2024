b1142ec0852036554b4b275aa2b06753497bbe44
Daniel Batista <dani_batista20@hotmail.com>
2024-05-13 09:05:17 +0100

    add: 39_confirmEnding

--- src/39_confirmEnding/confirmEnding.ts
+++ src/39_confirmEnding/confirmEnding.ts
@@ -1,6 +1,7 @@
-export function confirmEnding(str: string, target: string) {
-
+export function confirmEnding(str: string, target: string): boolean {
+  let res: string = str.substring(str.length - target.length)
+  return res === target
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding('Abstraction', 'action'))
+console.log(confirmEnding('Open sesame', 'pen'))
