a381a88944be9237ed5182c7d92ca0b3b9bccffc
Daniel Batista <dani_batista20@hotmail.com>
2024-05-13 11:33:50 +0100

    upd: 40_containsCloseNums - refactor

--- src/40_containsCloseNums/containsCloseNums.ts
+++ src/40_containsCloseNums/containsCloseNums.ts
@@ -1,13 +1,9 @@
-/* O(n^2) */
+/* O(n*k) */
 export function containsCloseNums(nums: number[], k: number): boolean {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i != j) {
-        if (nums[i] === nums[j]) {
-          if (Math.abs(i - j) <= k) {
-            return true
-          }
-        }
+    for (let j = i + 1; j <= i + k; j++) {
+      if (nums[i] === nums[j]) {
+        return true
       }
     }
   }
@@ -19,3 +15,19 @@ console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2))
 
 console.log(containsCloseNums([1, 2, 3, 1], 3))
 console.log(containsCloseNums([1, 2, 3, 1, 2, 3], 2))
+
+// /* O(n^2) */
+// export function containsCloseNums(nums: number[], k: number): boolean {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = 0; j < nums.length; j++) {
+//       if (i != j) {
+//         if (nums[i] === nums[j]) {
+//           if (Math.abs(i - j) <= k) {
+//             return true
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return false
+// }
