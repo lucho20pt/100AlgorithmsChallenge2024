96380d5586df2cfe5196709733798723fe52eab5
Daniel Batista <dani_batista20@hotmail.com>
2024-05-14 12:41:31 +0100

    add: 44_crossingSum

--- src/44_crossingSum/crossingSum.ts
+++ src/44_crossingSum/crossingSum.ts
@@ -1,7 +1,22 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
-
+  let sum: number = 0
+  const row = matrix[a].reduce((a, b) => a + b, 0)
+  for (let i = 0; i < matrix.length; i++) {
+    const col = i !== a ? matrix[i][b] : 0
+    sum += col
+  }
+  sum += row
+  return sum
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(
+  crossingSum(
+    [
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3]
+    ],
+    1,
+    3
+  )
+)
