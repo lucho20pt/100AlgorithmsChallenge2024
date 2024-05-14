bbf7c31e33912c6c765d667e3c5bdd9708f18f88
Daniel Batista <dani_batista20@hotmail.com>
2024-05-14 16:39:44 +0100

    add: 46_differentSymbolsNaive

--- src/46_differentSymbolsNaive/differentSymbolsNaive.ts
+++ src/46_differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,14 @@
 export function differentSymbolsNaive(s: string): number {
+  const sArr: string[] = s.split('')
+  const newArr: string[] = []
 
+  for (let i = 0; i < sArr.length; i++) {
+    if (!newArr.includes(sArr[i])) {
+      newArr.push(sArr[i])
+    }
+  }
+
+  return newArr.length
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'))
