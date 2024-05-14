8e473e82256bff9a994d400ddeab0f372335c44c
Daniel Batista <dani_batista20@hotmail.com>
2024-05-14 19:35:02 +0100

    add: 48_domainType

--- src/48_domainType/domainType.ts
+++ src/48_domainType/domainType.ts
@@ -1,5 +1,14 @@
 export function domainType(domains: string[]): string[] {
-
+  const domainType: string[] = []
+  domains.forEach((value) => {
+    if (value.endsWith('.org')) domainType.push('organization')
+    else if (value.endsWith('.com')) domainType.push('commercial')
+    else if (value.endsWith('.net')) domainType.push('network')
+    else if (value.endsWith('.info')) domainType.push('information')
+  })
+  return domainType
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(['en.wiki.org', 'codefights.com', 'happy.net', 'code.info'])
+)
