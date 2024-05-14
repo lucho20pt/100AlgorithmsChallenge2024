48180dfed122edb44292827e0242b282e9244b75
Daniel Batista <dani_batista20@hotmail.com>
2024-05-15 00:43:12 +0100

    upd: 49_electionWinners - tests

--- src/49_electionWinners/electionWinners.spec.ts
+++ src/49_electionWinners/electionWinners.spec.ts
@@ -1,15 +1,39 @@
-import { electionsWinners } from './electionWinners';
+import { electionsWinners } from './electionWinners'
 
 xdescribe(electionsWinners.name, () => {
-    it('Test 1', () => {
-        // arrange
-        const votes = [2, 3, 5, 2];
-        const k = 3;
+  it('Test 1', () => {
+    // arrange
+    const votes = [2, 3, 5, 2]
+    const k = 3
 
-        // act
-        const response = electionsWinners(votes, k);
+    // act
+    const response = electionsWinners(votes, k)
 
-        // assert 
-        expect(response).toBe(2);
-    });
-});
+    // assert
+    expect(response).toBe(2)
+  })
+
+  it('Test 2', () => {
+    // arrange
+    const votes = [2, 3, 9, 2]
+    const k = 3
+
+    // act
+    const response = electionsWinners(votes, k)
+
+    // assert
+    expect(response).toBe(1)
+  })
+
+  it('Test 3', () => {
+    // arrange
+    const votes = [2, 3, 3, 2]
+    const k = 3
+
+    // act
+    const response = electionsWinners(votes, k)
+
+    // assert
+    expect(response).toBe(4)
+  })
+})
