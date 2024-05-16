32efc0639d6360b9078452ef13b12138594ff223
Daniel Batista <dani_batista20@hotmail.com>
2024-05-16 22:50:23 +0100

    add: 61_firstDuplicate

--- src/61_firstDuplicate/firstDuplicate.ts
+++ src/61_firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,28 @@
 export function firstDuplicate(a: number[]): number {
+  let result = -1
+  let minIndex = -1
 
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i + 1; j < a.length; j++) {
+      const aa = a[i]
+      const bb = a[j]
+      
+      if (aa - bb === 0) {
+        let tempIndex = Math.abs(i - j)
+
+        if (minIndex === -1) {
+          minIndex = tempIndex
+          result = aa
+        } else if (minIndex > tempIndex) {
+          minIndex = tempIndex
+          result = aa
+        }
+      }
+    }
+  }
+  return result
 }
 
-// console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
-// console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([2, 1, 4, 5, 4, 2]))
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]))
+console.log(firstDuplicate([2, 4, 3, 5, 1]))
