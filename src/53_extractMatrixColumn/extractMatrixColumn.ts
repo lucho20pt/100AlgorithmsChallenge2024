a5e30abb48be9f425b75a59096bba7e0005b97c4
Daniel Batista <dani_batista20@hotmail.com>
2024-05-16 11:43:03 +0100

    add: 53_extractMatrixColumn

--- src/53_extractMatrixColumn/extractMatrixColumn.ts
+++ src/53_extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,24 @@
-export function extractMatrixColumn(matrix: number[][], column: number): number[] {
+export function extractMatrixColumn(
+  matrix: number[][],
+  column: number
+): number[] {
+  const arr: number[] = []
 
+  for (let i = 0; i < matrix.length; i++) {
+    let el = matrix[i][column]
+    arr.push(el)
+  }
+
+  return arr
 }
 
-// console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(
+  extractMatrixColumn(
+    [
+      [1, 1, 1, 2],
+      [0, 5, 0, 4],
+      [2, 1, 3, 6]
+    ],
+    2
+  )
+)
