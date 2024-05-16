954eaa38f553bfdf675e00e93a36e80a4cac56a1
Daniel Batista <dani_batista20@hotmail.com>
2024-05-16 21:42:51 +0100

    add: 60_firstDigit

--- src/60_firstDigit/firstDigit.ts
+++ src/60_firstDigit/firstDigit.ts
@@ -1,7 +1,17 @@
 export function firstDigit(inputString: string): string {
+  const strArr = inputString.split('')
+  let result = null
 
+  strArr.some((el) => {
+    const n = parseInt(el)
+    if (!isNaN(n)) {
+      result = el
+      return true
+    }
+  })
+  return result
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'))
+console.log(firstDigit('q2q-q'))
+console.log(firstDigit('0ss'))
