f9184cbb5418c9e70cf8657cf6a30be535aa1ed1
Daniel Batista <dani_batista20@hotmail.com>
2024-05-20 15:01:40 +0100

    add: 68_incorrectPasswordAttempts

--- src/68_incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ src/68_incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,20 @@
-export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+export function incorrectPasscodeAttempts(
+  passcode: string,
+  attempts: string[]
+): boolean {
+  const consecutiveAttempts = 10
+  let failedAttempt = 0
 
+  for (let i = 0; i < attempts.length; i++) {
+    if (passcode === attempts[i]) {
+      failedAttempt = 0
+    } else {
+      failedAttempt++
+    }
+    if (failedAttempt === consecutiveAttempts) return true
+  }
+
+  return false
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
