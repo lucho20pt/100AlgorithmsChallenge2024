40dd977daa192945219dd4ef32a173c11effa863
Daniel Batista <dani_batista20@hotmail.com>
2024-05-21 18:48:50 +0100

    add: 71_isLucky

--- src/71_isLucky/isLucky.ts
+++ src/71_isLucky/isLucky.ts
@@ -1,6 +1,15 @@
 export function isLucky(n: number): boolean {
+  const tempArr = n.toString().split('')
+  const halfLength = Math.floor(tempArr.length / 2)
 
+  const leftArr = tempArr.slice(0, halfLength)
+  const rightArr = tempArr.slice(halfLength)
+
+  const left = leftArr.reduce((acc, curr) => acc + parseInt(curr), 0)
+  const right = rightArr.reduce((acc, curr) => acc + parseInt(curr), 0)
+
+  return left === right ? true : false
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+console.log(isLucky(1230))
+console.log(isLucky(239017))
