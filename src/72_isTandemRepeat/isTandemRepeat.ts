f72242cd16a1ae221bb92b8992b8bf9f72b69eb5
Daniel Batista <dani_batista20@hotmail.com>
2024-05-21 19:03:31 +0100

    add: 72_isTandemRepeat

--- src/72_isTandemRepeat/isTandemRepeat.ts
+++ src/72_isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,10 @@
 export function isTandemRepeat(inputString: string): boolean {
+  const length: number = Math.floor(inputString.length)
+  const left: string = inputString.substring(0, length / 2)
 
+  return left.concat(...left) === inputString ? true : false
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'))
+console.log(isTandemRepeat('qqq'))
+console.log(isTandemRepeat('2w2ww'))
