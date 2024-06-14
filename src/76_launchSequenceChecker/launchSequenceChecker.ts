0dc5210f04fc29916f6172ed99ca3d78db6aa310
Daniel Batista <dani_batista20@hotmail.com>
2024-06-14 16:15:27 +0100

    add: 76_launchSequenceChecker

--- src/76_launchSequenceChecker/launchSequenceChecker.ts
+++ src/76_launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,32 @@
-export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+export function launchSequenceChecker(
+  systemNames: string[],
+  stepNumbers: number[]
+): boolean {
+  //
+  const obj: { [key: string]: number } = {}
 
+  for (let i = 0; i < systemNames.length; i++) {
+    const system = systemNames[i]
+    const step = stepNumbers[i]
+
+    if (!obj.hasOwnProperty(system)) {
+      obj[system] = step
+    } else if (obj[system].valueOf() >= step) {
+      return false
+    }
+  }
+  return true
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+console.log(
+  launchSequenceChecker(
+    ['stage_1', 'stage_2', 'dragon', 'stage_1', 'stage_2', 'dragon'],
+    [1, 10, 11, 2, 12, 111]
+  )
+)
+console.log(
+  launchSequenceChecker(
+    ['stage_1', 'stage_1', 'stage_2', 'dragon'],
+    [2, 1, 12, 111]
+  )
+)
