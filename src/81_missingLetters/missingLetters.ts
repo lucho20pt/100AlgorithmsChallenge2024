8fbd2774f58b6425453dd20029f5d647b8eef2a5
Daniel Batista <dani_batista20@hotmail.com>
2024-06-25 15:23:35 +0100

    add: 81_missingLetters

--- src/81_missingLetters/missingLetters.ts
+++ src/81_missingLetters/missingLetters.ts
@@ -1,8 +1,19 @@
 export function missingLetters(str: string): string {
+  const strArray: string[] = str.split('')
+  let temp: number = strArray[0].charCodeAt(0)
 
+  for (const char of strArray) {
+    const curr = Number(char.charCodeAt(0))
+
+    if (temp !== curr) {
+      return String.fromCharCode(temp)
+    } else {
+      temp++
+    }
+  }
 }
 
-// console.log(missingLetters("bce"));
-// console.log(missingLetters("abce"));
-// console.log(missingLetters("abcdefghjklmno"));
-// console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters('bce'))
+console.log(missingLetters('abce'))
+console.log(missingLetters('abcdefghjklmno'))
+console.log(missingLetters('abcdefghijklmnopqrstuvwxyz'))
