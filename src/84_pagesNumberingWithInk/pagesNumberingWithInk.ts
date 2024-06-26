c7ed954d80f823bc2a4cbc1f4335d38084195884
Daniel Batista <dani_batista20@hotmail.com>
2024-06-26 13:23:06 +0100

    add: 84_pagesNumberingWithInk

--- src/84_pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ src/84_pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,21 @@
-export function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+export function pagesNumberingWithInk(
+  current: number,
+  numberOfDigits: number
+): number {
+  let sumCurrentSize = current.toString().length
+  let lastPrintedPage = null
 
+  for (let i = 0; i < numberOfDigits; i++) {
+    const currentSize = current.toString().length
+
+    lastPrintedPage = current
+    sumCurrentSize += currentSize
+    current++
+
+    if (sumCurrentSize > numberOfDigits) return lastPrintedPage
+  }
 }
 
-// console.log(pagesNumberingWithInk('G'));
+console.log(pagesNumberingWithInk(1, 5))
+console.log(pagesNumberingWithInk(21, 5))
+console.log(pagesNumberingWithInk(8, 4))
