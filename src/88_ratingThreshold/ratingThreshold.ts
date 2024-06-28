0d6b4f0a4a7821433f098a688a46f8672e97ea98
Daniel Batista <dani_batista20@hotmail.com>
2024-06-28 16:42:22 +0100

    add: 88_ratingThreshold

--- src/88_ratingThreshold/ratingThreshold.ts
+++ src/88_ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,24 @@
-export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+export function ratingThreshold(
+  threshold: number,
+  ratings: number[][]
+): number[] {
+  //
+  const toBeReviewed: number[] = []
 
+  ratings.forEach((row, r) => {
+    let sum: number = null
+
+    row.forEach((col, c) => {
+      sum += col
+    })
+
+    const rate = sum / row.length
+    if (threshold > rate) {
+      toBeReviewed.push(r)
+    }
+  })
+
+  return toBeReviewed
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5, [[3, 4], [3, 3, 3, 4], [4]]))
