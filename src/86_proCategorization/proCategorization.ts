53564fc30df9a93d7057799cd4549203b7cc482d
Daniel Batista <dani_batista20@hotmail.com>
2024-06-28 15:26:10 +0100

    add: 86_proCategorization

--- src/86_proCategorization/proCategorization.ts
+++ src/86_proCategorization/proCategorization.ts
@@ -1,7 +1,52 @@
-export function proCategorization(pros: string[], preferences: string[][]): string[][][] {
+/**
+ * Categorizes professionals based on their preferences.
+ *
+ * @param pros - An array of professional names.
+ * @param preferences - A 2D array of job preferences for each professional.
+ * @returns A 3D array representing the categorized professionals.
+ */
 
+export function proCategorization(
+  pros: string[],
+  preferences: string[][]
+): string[][][] {
+  const categories: { [key: string]: string[] } = {}
+
+  // filter unique categories
+  preferences.forEach((row, r) => {
+    row.forEach((job, j) => {
+      if (!categories.hasOwnProperty(job)) {
+        categories[job] = Array(pros[r])
+      } else {
+        categories[job] = Array(...categories[job], pros[r])
+      }
+    })
+  })
+
+  // order by categories
+  const ordered: { [key: string]: string[] } = Object.keys(categories)
+    .sort()
+    .reduce((obj: { [key: string]: string[] }, key: string) => {
+      obj[key] = categories[key]
+      return obj
+    }, {})
+
+  // construct result as requested format
+  const result: string[][][] = Object.entries(ordered).map(([job, pros]) => [
+    [job],
+    pros,
+  ])
+
+  return result
 }
 
-// console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
-// ["Computer lessons", "Computer repair", "Data recovery service"],
-// ["Computer lessons", "House cleaning"]]));
+console.log(
+  proCategorization(
+    ['Jack', 'Leon', 'Maria'],
+    [
+      ['Computer repair', 'Handyman', 'House cleaning'],
+      ['Computer lessons', 'Computer repair', 'Data recovery service'],
+      ['Computer lessons', 'House cleaning'],
+    ]
+  )
+)
